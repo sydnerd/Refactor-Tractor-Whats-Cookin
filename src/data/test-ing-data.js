e24bd53d4d6c027210f5beba087e47252ab51f34
Erica <ericaspitz522@gmail.com>
2021-06-02 15:31:19 -0600

    Add test ingredient file

---
+++ src/data/test-ing-data.js
@@ -0,0 +1,18 @@
+let ingredientsData = [
+    {
+      "id": 20081,
+      "name": "wheat flour",
+      "estimatedCostInCents": 142
+    },
+    {
+      "id": 18372,
+      "name": "bicarbonate of soda",
+      "estimatedCostInCents": 582
+    },
+    {
+      "id": 1123,
+      "name": "eggs",
+      "estimatedCostInCents": 472
+    }
+]
+  
